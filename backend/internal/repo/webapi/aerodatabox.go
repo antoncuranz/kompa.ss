8692a126b3efe62e22a473a8877c569c6167924d
Anton Curanz <anton@curanz.de>
2025-08-06 17:42:51 +0200

    error handling

--- backend/internal/repo/webapi/aerodatabox.go
+++ backend/internal/repo/webapi/aerodatabox.go
@@ -30,7 +30,7 @@ func (a *AerodataboxWebAPI) RetrieveFlightLeg(ctx context.Context, date string, 
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		return entity.FlightLeg{}, err
+		return entity.FlightLeg{}, fmt.Errorf("create http request: %w", err)
 	}
 
 	req.Header.Add("x-rapidapi-key", a.apiKey)
@@ -38,66 +38,64 @@ func (a *AerodataboxWebAPI) RetrieveFlightLeg(ctx context.Context, date string, 
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return entity.FlightLeg{}, err
-	} else if res.StatusCode < 200 || res.StatusCode >= 300 {
-		return entity.FlightLeg{}, errors.New("Error calling Aerodatabox API, status: " + res.Status)
+		return entity.FlightLeg{}, fmt.Errorf("do http request: %w", err)
 	} else if res.StatusCode != 200 {
-		return entity.FlightLeg{}, errors.New("AerodataboxWebAPI - RetrieveFlightLeg - no results")
+		return entity.FlightLeg{}, fmt.Errorf("http status code %d: %w", res.StatusCode, err)
 	}
 
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return entity.FlightLeg{}, err
+		return entity.FlightLeg{}, fmt.Errorf("read response body: %w", err)
 	}
 
-	var data []response.FlightContract
-	if err := json.Unmarshal(body, &data); err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
-		return entity.FlightLeg{}, err
+	var flightContracts []response.FlightContract
+	if err := json.Unmarshal(body, &flightContracts); err != nil {
+		return entity.FlightLeg{}, fmt.Errorf("unmarshall JSON: %w", err)
 	}
 
-	if len(data) == 0 {
-		return entity.FlightLeg{}, errors.New("AerodataboxWebAPI - RetrieveFlightLeg - no results")
+	if len(flightContracts) == 0 {
+		return entity.FlightLeg{}, fmt.Errorf("no flight contract found")
 	}
 
 	if origin == nil {
-		return mapFlightLeg(data[0])
+		return mapFlightLeg(flightContracts[0])
 	}
 
-	for _, flight := range data {
+	for _, flight := range flightContracts {
 		if *flight.Departure.Airport.Iata == *origin {
 			return mapFlightLeg(flight)
 		}
 	}
 
-	return entity.FlightLeg{}, errors.New("AerodataboxWebAPI - RetrieveFlightLeg - no results")
+	return entity.FlightLeg{}, fmt.Errorf("no flight contract found with origin: %s", *origin)
 }
 
 func mapFlightLeg(flightContract response.FlightContract) (entity.FlightLeg, error) {
+	if flightContract.Departure.ScheduledTime == nil || flightContract.Arrival.ScheduledTime == nil {
+		return entity.FlightLeg{}, fmt.Errorf("flight leg does not have scheduled time")
+	}
+
 	departureDateTime, err1 := parseLocalDateTime(flightContract.Departure.ScheduledTime.Local)
 	arrivalDateTime, err2 := parseLocalDateTime(flightContract.Arrival.ScheduledTime.Local)
 	if err := errors.Join(err1, err2); err != nil {
-		fmt.Printf("Error parsing timestamp: departure=%s, arrival=%s\n", departureDateTime, arrivalDateTime)
-		return entity.FlightLeg{}, err
+		return entity.FlightLeg{}, fmt.Errorf("parse timestamps: %w", err)
 	}
 
 	durationInMinutes, err := calculateDuration(flightContract.Departure.ScheduledTime.UTC, flightContract.Arrival.ScheduledTime.UTC)
 	if err != nil {
-		fmt.Println("Error Calculating duration of flight leg.")
-		return entity.FlightLeg{}, err
+		return entity.FlightLeg{}, fmt.Errorf("calculate duration: %w", err)
 	}
 
 	return entity.FlightLeg{
 		Origin:            mapAirport(flightContract.Departure.Airport),
 		Destination:       mapAirport(flightContract.Arrival.Airport),
-		Airline:           flightContract.Airline.Name,
+		Airline:           flightContract.Airline.Name, // FIXME
 		FlightNumber:      flightContract.Number,
 		DepartureDateTime: departureDateTime,
 		ArrivalDateTime:   arrivalDateTime,
 		DurationInMinutes: durationInMinutes,
-		Aircraft:          flightContract.Aircraft.Model,
+		Aircraft:          flightContract.Aircraft.Model, // FIXME
 	}, nil
 }
 
@@ -105,8 +103,7 @@ func calculateDuration(fromUtcTimestamp string, toUtcTimestamp string) (int32, e
 	from, err1 := parseTimestamp(fromUtcTimestamp)
 	to, err2 := parseTimestamp(toUtcTimestamp)
 	if err := errors.Join(err1, err2); err != nil {
-		fmt.Printf("Error parsing timestamp: from=%s, to=%s\n", from, to)
-		return 0, err
+		return 0, fmt.Errorf("parse timestamps: %w", err)
 	}
 
 	return int32(to.Sub(from).Minutes()), nil
@@ -114,7 +111,7 @@ func calculateDuration(fromUtcTimestamp string, toUtcTimestamp string) (int32, e
 
 func parseTimestamp(timestamp string) (time.Time, error) {
 	format := "2006-01-02 15:04Z"
-	return time.Parse(format, timestamp)
+	return time.Parse(format, timestamp) // error already contains timestamp and format
 }
 
 func parseLocalDateTime(timestamp string) (civil.DateTime, error) {
@@ -122,14 +119,14 @@ func parseLocalDateTime(timestamp string) (civil.DateTime, error) {
 	timestamp = timestamp[0:16] + ":00"
 	// Separator between date and time needs to be a T
 	timestamp = strings.Replace(timestamp, " ", "T", 1)
-	return civil.ParseDateTime(timestamp)
+	return civil.ParseDateTime(timestamp) // error already contains timestamp and format
 }
 
 func mapAirport(airport response.ListingAirportContract) entity.Airport {
 	return entity.Airport{
-		Iata:         *airport.Iata,
+		Iata:         *airport.Iata, // FIXME
 		Name:         airport.Name,
-		Municipality: *airport.MunicipalityName,
+		Municipality: *airport.MunicipalityName, // FIXME
 		Location: &entity.Location{
 			Latitude:  airport.Location.Lat,
 			Longitude: airport.Location.Lon,
