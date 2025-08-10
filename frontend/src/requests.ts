f17052d507cd11d77b2362dbe14ba9c778478de4
Anton Curanz <anton@curanz.de>
2025-08-10 20:14:58 +0200

    timezone problems

--- frontend/src/requests.ts
+++ frontend/src/requests.ts
@@ -42,8 +42,8 @@ export async function fetchTrip(tripId: number) {
   const trip = await fetchData("/api/v1/trips/" + tripId) as Trip
   return {
     ...trip,
-    startDate: new Date(trip.startDate),
-    endDate: new Date(trip.endDate),
+    startDate: parseLocalDateTime(trip.startDate),
+    endDate: parseLocalDateTime(trip.endDate),
   }
 }
 
@@ -51,8 +51,8 @@ export async function fetchTrips() {
   const trips = await fetchData("/api/v1/trips") as Trip[]
   return trips.map(trip => ({
     ...trip,
-    startDate: new Date(trip.startDate),
-    endDate: new Date(trip.endDate),
+    startDate: parseLocalDateTime(trip.startDate),
+    endDate: parseLocalDateTime(trip.endDate),
   }))
 }
 
@@ -60,8 +60,8 @@ export async function fetchAccommodation(tripId: number) {
   const accommodation = await fetchData("/api/v1/trips/" + tripId + "/accommodation") as Accommodation[]
   return accommodation.map(acc => ({
     ...acc,
-    arrivalDate: new Date(acc.arrivalDate),
-    departureDate: new Date(acc.departureDate)
+    arrivalDate: parseLocalDateTime(acc.arrivalDate),
+    departureDate: parseLocalDateTime(acc.departureDate)
   }))
 }
 
@@ -71,8 +71,8 @@ export async function fetchFlights(tripId: number) {
     ...flight,
     legs: flight.legs.map(leg => ({
       ...leg,
-      departureDateTime: new Date(leg.departureDateTime),
-      arrivalDateTime: new Date(leg.arrivalDateTime)
+      departureDateTime: parseLocalDateTime(leg.departureDateTime),
+      arrivalDateTime: parseLocalDateTime(leg.arrivalDateTime)
     }))
   }))
 }
@@ -84,4 +84,11 @@ export async function fetchActivities(tripId: number) {
     date: new Date(activity.date),
     time: activity.time ? new Date("1970-01-01T" + activity.time) : null
   }))
+}
+
+function parseLocalDateTime(timestamp: Date|string): Date {
+  // timestamp is actually a string, but don't tell typescript!
+  const date = new Date(timestamp)
+  const userTimezoneOffset = date.getTimezoneOffset() * 60000;
+  return new Date(date.getTime() + userTimezoneOffset);
 }
